a46d77eefc284576a47e67dc520a097828cbef34
piyx <razorabhishek007@gmail.com>
2020-10-31 20:02:27 +0530

    added client, utils and tracks

---
+++ spotify_client_manager.py
@@ -0,0 +1,26 @@
+from spotipy import Spotify
+from spotipy import util
+from song import Song
+import os
+
+
+class SpotifyClientManager:
+    def __init__(self):
+        self.scope = 'user-library-read'
+        self.user_id = os.getenv('SPOTIFY_USER_ID')
+        self.client_id = os.getenv('SPOTIFY_CLIENT_ID')
+        self.client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+        self.redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI')
+
+    @property
+    def get_token(self):
+        '''
+        Return the access token
+        '''
+        return util.prompt_for_user_token(
+            self.user_id,
+            scope=self.scope,
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            redirect_uri=self.redirect_uri
+        )
