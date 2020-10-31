94859996c3554caf571c32cf85da4b5ad5577a55
piyx <razorabhishek007@gmail.com>
2020-10-31 11:08:00 +0530

    added custom errors

---
+++ errors.py
@@ -0,0 +1,22 @@
+class YoutubeDownloadError(Exception):
+    pass
+
+
+class ConvertToMp3Error(Exception):
+    pass
+
+
+class YoutubeSongSearchError(Exception):
+    pass
+
+
+class SpotifyAuthenticationFailed(Exception):
+    pass
+
+
+class ID3TagError(Exception):
+    pass
+
+
+class SpotifyInfoNotFoundError(Exception):
+    pass
