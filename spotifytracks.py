2d61bc052e372044da553122300ab886f1de90cd
piyx <v1rtu5l.zx@gmail.com>
2020-11-02 19:12:41 +0530

    fixed docs

--- spotifytracks.py
+++ spotifytracks.py
@@ -9,6 +9,16 @@ client_manager = SpotifyClientManager()
 
 
 class Song:
+    '''
+    Song class to store useful track information
+    Eg: Song(
+        title='hold on',
+        artist='flor',
+        album="come out. you're hiding (deluxe)",
+        image='https://i.scdn.co/image/ab67616d0000b273b7f72643bf8a029abeacc5dd'
+    )
+    '''
+
     def __init__(self, title, artist, album, image):
         self.title = title
         self.artist = artist
@@ -19,7 +29,7 @@ class Song:
         return f'Song({self.title}, {self.artist}, {self.album}, {self.image})'
 
     def __str__(self):
-        return self.title
+        return f'{self.title} - {self.artist}'
 
 
 class SpotifyTracks:
@@ -27,10 +37,6 @@ class SpotifyTracks:
         self.spotify = Spotify(auth=client_manager.get_token)
 
     def get_cleaned_tracks_data(self, results):
-        '''
-        Returns a list of songs given a list of track IDs
-        song -> contains useful track data
-        '''
         ''' Get useful information from the results
 
         Parameters:
@@ -52,8 +58,8 @@ class SpotifyTracks:
         ''' Get a list of tracks of a playlist.
 
         Parameters:
-            - playlist_id : the id of the playlist
-            - limit       : the number of tracks to return (max = 50, default = 10)
+            - playlist_id: the id of the playlist
+            - limit: the number of tracks to return (max=50, default=10)
         '''
         limit = min(50, limit)
         query = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
@@ -72,8 +78,8 @@ class SpotifyTracks:
         ''' Get a list of the user saved tracks.
 
         Parameters:
-            - playlist_id : the id of the playlist
-            - limit       : the number of tracks to return (max = 50, default = 10)
+            - playlist_id: the id of the playlist
+            - limit: the number of tracks to return (max=50, default=10)
         '''
         limit = min(50, limit)
         results = self.spotify.current_user_saved_tracks(limit=limit)
@@ -83,8 +89,8 @@ class SpotifyTracks:
         ''' Get a particular track info
 
         Parameters:
-            - artist_name : the name of the artist
-            - song_name   : the name of the song
+            - artist_name: the name of the artist
+            - song_name: the name of the song
         '''
         results = self.spotify.search(
             q=f'artist:{artist_name} track:{song_name}',
@@ -100,3 +106,12 @@ if __name__ == "__main__":
     pprint.pprint(sp.get_playlist_tracks('37i9dQZF1DXcRXFNfZr7Tp'))
     pprint.pprint(sp.search_track('flor', 'hold on'))
     pprint.pprint(sp.get_user_saved_tracks(limit=100))
+
+    song = Song(
+        title='hold on',
+        artist='flor',
+        album="come out. you're hiding(deluxe)",
+        image='https://i.scdn.co/image/ab67616d0000b273b7f72643bf8a029abeacc5dd'
+    )
+    print(repr(song))
+    print(str(song))
