8d34d5b538fb72f9944c220f21e8ac219e01cd0a
dejan995 <40325303+dejan995@users.noreply.github.com>
2021-07-04 21:07:23 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Declare variables to be available to Docker.
+export SPOTIFY_USER_ID="${SPOTIFY_USER_ID}"
+export SPOTIFY_CLIENT_ID="${SPOTIFY_CLIENT_ID}"
+export SPOTIFY_CLIENT_SECRET="${SPOTIFY_CLIENT_SECRET}"
+export SPOTIFY_REDIRECT_URI="${SPOTIFY_REDIRECT_URI}"
+
+# Generate .env file just in case the above fails for some reason.
+cat << EOF > .env
+
+SPOTIFY_USER_ID=${SPOTIFY_USER_ID}
+SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID}
+SPOTIFY_CLIENT_SECRET=${SPOTIFY_CLIENT_SECRET}
+SPOTIFY_REDIRECT_URI=${SPOTIFY_REDIRECT_URI}
+
+EOF
+
+# Expose app files to the host.
+if [ ! -d "/app" ]; then
+  ln -s /opt/app/* /app
+elif [ -z "$(ls -A /app)" ]; then
+  cp -a /opt/app/. /app
+fi
+
+exec "$@"
